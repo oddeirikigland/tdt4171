3c79e12dd27dccbbb5caca379cceaa1e5e6bbb39
Odd Eirik Igland <oddeirik.igland@gmail.com>
2019-02-20 11:24:40 +0100

    implemented backward, algorithm complete

--- ov2/ov2.py
+++ ov2/ov2.py
@@ -1,24 +1,44 @@
 import numpy as np
 
+np.set_printoptions(precision=3)
+
+
+def normalize(np_array):
+    norm = lambda x: x / np.sum(np_array)
+    return norm(np_array)
+
 
 def forward(currentState, evidence, transitionModel):
-    prediction = np.matmul(evidence, transitionModel.dot(currentState))
-    normalize = lambda x: x / np.sum(prediction)
-    return normalize(prediction)
+    return normalize(np.matmul(evidence, transitionModel.dot(currentState)))
+
+
+def backward(currentState, evidence, transitionModel):
+    return normalize(np.matmul(currentState, np.matmul(evidence, transitionModel)))
+
+
+def smoothed(forward, backward):
+    out = []
+    for f, b in zip(forward, backward):
+        out.append(normalize(f * b))
+    return out
+
+
+def nicePrint(msg, arr):
+    print(msg)
+    for elem in arr:
+        print(elem)
 
 
 def calculateProbability(ev, transitionModel):
     fv = [np.array([0.5, 0.5])]
-    t = len(ev)
-    for i in range(1, t):
-        # print(
-        #     "0:{}, evi: {}, trans: {}, curr: {}\n".format(
-        #         i, ev[i], transitionModel, fv[i - 1]
-        #     )
-        # )
+    bv = [np.array([1.0, 1.0])]
+    for i in range(1, len(ev)):
         fv.append(forward(fv[i - 1], ev[i], transitionModel))
-    print("Probability of rain at day {}: {}".format(t - 1, round(fv[t - 1][0], 3)))
-
+        bv.append(backward(bv[i - 1], ev[i], transitionModel))
+    smooth = smoothed(fv, np.flipud(bv)) # Reversing the backward calculation with np.flipud()
+    nicePrint("\nNormalized forward messages: ", fv)
+    nicePrint("\nNormalized backward messages: ", smooth)
+    return smooth[-1]
 
 
 def main():
